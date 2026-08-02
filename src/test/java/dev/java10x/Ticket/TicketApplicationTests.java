f0f7916f2b2547c64cd8e7752f5ad96efa6a153f
Guilherme Sousa Borges <guilhermepllay157@gmail.com>
2026-08-02 13:22:47 -0300

    initial commit

---
+++ src/test/java/dev/java10x/Ticket/TicketApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.Ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
