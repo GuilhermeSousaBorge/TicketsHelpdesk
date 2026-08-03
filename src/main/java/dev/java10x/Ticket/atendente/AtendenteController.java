fc3eaff0619601ad479e911d9ab6ab63d0dae067
Guilherme Sousa Borges <guilhermepllay157@gmail.com>
2026-08-03 10:48:41 -0300

    feat: criado mock de dados das rotas de atendente

--- src/main/java/dev/java10x/Ticket/atendente/AtendenteController.java
+++ src/main/java/dev/java10x/Ticket/atendente/AtendenteController.java
@@ -1,8 +1,6 @@
 package dev.java10x.Ticket.atendente;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,6 +9,31 @@ public class AtendenteController {
     @GetMapping("/boas-vindas")
     public String BoasVindas(){
         return "Hello World!";
+    }
+
+    @PostMapping("/criar")
+    public String criarAtendente(){
+        return "Atendente criado com sucesso!";
+    }
+
+    @GetMapping("/todos")
+    public String mostrarTodos(){
+        return "Hello World!";
+    }
+
+    @GetMapping("/atendenteId")
+    public String mostrarAtendentePorId(){
+        return "Atendente encontrado!";
+    }
+
+    @PutMapping("/editarId")
+    public String editarAtendentePorId(){
+        return "alterar atendente por id";
+    }
+
+    @DeleteMapping("/deletarId")
+    public String deletarAtendentePorId(){
+        return "deletar atendente por id";
     }
 
 }
