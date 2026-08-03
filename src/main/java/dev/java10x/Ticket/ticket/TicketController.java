0d1feaedf6aa2d67db7a95b4758b09d696405f96
Guilherme Sousa Borges <guilhermepllay157@gmail.com>
2026-08-03 20:37:52 -0300

    feat: adicionado a logica do CRUD para tickets

--- src/main/java/dev/java10x/Ticket/ticket/TicketController.java
+++ src/main/java/dev/java10x/Ticket/ticket/TicketController.java
@@ -2,32 +2,41 @@ package dev.java10x.Ticket.ticket;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("tickets")
 public class TicketController {
 
+    private final TicketService ticketService;
+
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
     @PostMapping("/criar")
-    public String criarTicket(){
-        return "Ticket criado com sucesso!";
+    public TicketModel criarTicket(@RequestBody TicketModel ticketModel){
+        return ticketService.criarTicket(ticketModel);
     }
 
     @GetMapping("/todos")
-    public String mostrarTodos(){
-        return "Hello World!";
+    public List<TicketModel> mostrarTodos(){
+        return ticketService.listarTickets();
     }
 
-    @GetMapping("/ticketId")
-    public String mostrarTicketPorId(){
-        return "Ticket encontrado!";
+    @GetMapping("/{id}")
+    public TicketModel mostrarTicketPorId(@PathVariable Long id){
+        return ticketService.buscarTicketPorId(id);
     }
 
-    @PutMapping("/editarId")
-    public String editarTicketPorId(){
-        return "alterar Ticket por id";
+    @PutMapping("/{id}")
+    public TicketModel editarTicketPorId(@PathVariable Long id, @RequestBody TicketModel ticketModel){
+        return ticketService.editarTicket(id, ticketModel);
     }
 
-    @DeleteMapping("/deletarId")
-    public String deletarTicketPorId(){
-        return "deletar Ticket por id";
+    @DeleteMapping("/{id}")
+    public TicketModel deletarTicketPorId(@PathVariable Long id){
+        ticketService.apagarTicket(id);
+        return new TicketModel();
     }
 }
--- src/main/java/dev/java10x/Ticket/ticket/TicketService.java
+++ src/main/java/dev/java10x/Ticket/ticket/TicketService.java
@@ -1,4 +1,40 @@
 package dev.java10x.Ticket.ticket;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TicketService {
+
+    private final TicketRepository ticketRepository;
+
+    public TicketService(TicketRepository ticketRepository) {
+        this.ticketRepository = ticketRepository;
+    }
+
+    public TicketModel criarTicket(TicketModel ticketModel) {
+        return ticketRepository.save(ticketModel);
+    }
+
+    public List<TicketModel> listarTickets() {
+        return ticketRepository.findAll();
+    }
+
+    public TicketModel buscarTicketPorId(Long id) {
+        return ticketRepository.findById(id).orElseThrow(() -> new RuntimeException("Ticket não encontrado"));
+    }
+
+    public TicketModel editarTicket(Long id, TicketModel ticketModel){
+
+        TicketModel ticket = ticketRepository.findById(id).orElseThrow(() -> new RuntimeException("Ticket não encontrado"));
+        ticket.setTitulo(ticketModel.getTitulo());
+        ticket.setDescricao(ticketModel.getDescricao());
+        ticket.setStatus(ticketModel.getStatus());
+        return ticketRepository.save(ticket);
+    }
+
+    public void apagarTicket(Long id){
+        ticketRepository.deleteById(id);
+    }
 }
