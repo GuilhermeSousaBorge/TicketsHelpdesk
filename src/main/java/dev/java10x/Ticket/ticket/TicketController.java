ca48b7d99c926c783c4c2a152ec3a212c2053e5e
Guilherme Sousa Borges <guilhermepllay157@gmail.com>
2026-08-03 11:04:59 -0300

    feat: criado mock de dados das rotas de tickets

--- src/main/java/dev/java10x/Ticket/ticket/TicketController.java
+++ src/main/java/dev/java10x/Ticket/ticket/TicketController.java
@@ -1,4 +1,33 @@
 package dev.java10x.Ticket.ticket;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("tickets")
 public class TicketController {
+
+    @PostMapping("/criar")
+    public String criarTicket(){
+        return "Ticket criado com sucesso!";
+    }
+
+    @GetMapping("/todos")
+    public String mostrarTodos(){
+        return "Hello World!";
+    }
+
+    @GetMapping("/ticketId")
+    public String mostrarTicketPorId(){
+        return "Ticket encontrado!";
+    }
+
+    @PutMapping("/editarId")
+    public String editarTicketPorId(){
+        return "alterar Ticket por id";
+    }
+
+    @DeleteMapping("/deletarId")
+    public String deletarTicketPorId(){
+        return "deletar Ticket por id";
+    }
 }
